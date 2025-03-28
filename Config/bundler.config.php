a4c51a25d2b0cd0deb55e5b3b3044c0e8d6d2df4
Diogo Graciano <diogo.dg691@gmail.com>
2025-03-28 15:59:15 -0300

    Atualização e Documentação

--- Config/bundler.config.php
+++ Config/bundler.config.php
@@ -2,12 +2,7 @@
 
 return [
     "js" => [
-        "homepage" => ["htmx.min.js","tailwindcss.js","flowbite.min.js","swiper-bundle.min.js","zmain.js"],
-        "nohomepage" => ["htmx.min.js","tailwindcss.js","flowbite.min.js","zmain.js"],
-        "pagamentos" => ["htmx.min.js","tailwindcss.js","flowbite.min.js","choices.min.js","card.js","zmain.js"],
-        "dashboard" => ["htmx.min.js","tailwindcss.js","flowbite.min.js","choices.min.js","sweetalert2.all.min.js","zmain.js"]
     ],
     "css" => [
-        "site" =>  ['all.min.css','aos.css','choices.min.css','swiper-bundle.min.css','tailwind.css']
     ]
 ];
--- public/index.php
+++ public/index.php
@@ -1,31 +1,9 @@
 <?php
+
 require '../bootstrap.php';
 
-use Core\Router;
-use Core\Session;
-use App\View\Layout\Error;
-use App\View\Layout\Head;
-use Core\Logger;
-use Core\Response;
+use NeoFramework\Core\Kernel;
 
-Session::start();
-date_default_timezone_set('America/Sao_Paulo');
-
-$whoops = new \Whoops\Run;
-if ($_ENV["ENVIRONMENT"] !== "prod") {
-    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-} else {
-    $whoops->pushHandler(function($e){
-        Logger::error('Error: '.$e->getMessage().' Trace: '.$e->getTraceAsString());
-        $response = new Response;
-        $response->setCode($e->getCode()?:500);
-        $response->addContent(new Head("Error"));
-        $response->addContent(new Error($e->getCode()?:500,!$e->getCode()?"Erro ao processar requisição":$e->getMessage()));
-        $response->send();
-    });
-}
-$whoops->register();
-
-(new Router)->load();
+Kernel::init();
 
 ?>
