e4a2bc2757ae0a4afe8a049fd20e0fa3c9e7c238
Diogo Graciano <diogo.dg691@gmail.com>
2025-04-15 11:08:40 -0300

    Atualizado v1.1.2

--- bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__.DIRECTORY_SEPARATOR."vendor".DIRECTORY_SEPARATOR."autoload.php";
-
-$_ENV = parse_ini_file('.env');
-
-error_reporting(E_ALL);
-
-?>
--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require '../bootstrap.php';
+require '../vendor/autoload.php';
 
 use NeoFramework\Core\Kernel;
 
